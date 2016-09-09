47301af26e64ac139202dd466b8e9735d2673f72
elisehellwig <echellwig@ucdavis.edu>
2016-09-09 13:17:10 -0700

    added documentation to extracttemp

--- R/general.R
+++ R/general.R
@@ -1,31 +1,59 @@
-extracttemp <- function(tdat, years, starts, ends, tempname=NA, 
+#' Extracts temperatures from a data frame
+#'
+#' This function extracts temperature data from a data frame for specific days
+#'     in a year over a given set of years. It is used with the yearsums and
+#'     minrmse functions.
+#'
+#' @param tdat a data frame containing the temperature data.
+#' @param years a vector of years over which to extract temperature data.
+#' @param starts a number or vector of numbers between 1 and 365 (inclusive)
+#'     specifying what days of the year to begin the temperature data extraction.
+#' @param ends a number or vector of numbers between 1 and 365 (inclusive)
+#'     specifying what days of the year to end the temperature data extraction.
+#' @param tempname name of the column that contains the temperature data. The
+#'      function will try and detect the name of the column if tempname is left
+#'      as NA.
+#' @param yearname the name of the column that specifies the year.
+#' @param dayname the name of the column that specifies the day of the year,
+#'     numeric.
+#' @return \code{extracttemp} returns a list the same length as \code{years},
+#'     where each element of the list is a vector of all the temperatures in
+#'     between \code{starts} and \code{ends} that year.
+#' @details If tempname is left as NA the function will try to detect the
+#'     correct column (either temp, tmin or tmax). However, it is safer to just
+#'     specify the correct column name.
+#' @export
+extracttemp <- function(tdat, years, starts, ends, tempname=NA,
                         yearname='year', dayname='day') {
-	
-    #print(1)
-    
+
+
+    if (length(starts)==1) { #checks if there is a start day for each year
+        starts <- rep(starts, length(years))
+    }
+
     if (length(ends)==1) {
         ends <- rep(ends, length(starts))
     }
-    
+
     #print(years)
-    
+
     if (is.na(tempname)) {
-        
+
         if ('temp' %in% names(tdat)) {
             tnames <- 'temp'
-            
+
         } else if ('tmin' %in% names(tdat) & 'tmax' %in% names(tdat)) {
             tnames <- c('tmin','tmax')
-            
+
         } else {
-            stop('The names of the variables with temperature data must be 
+            stop('The names of the variables with temperature data must be
                     temp, tmin and tmax, or it must be specified with the
                     tempname argument.')
         }
     } else {
         tnames <- tempname
     }
-    
+
     tlist <- lapply(1:length(years), function(i) {
         rows <- which(tdat[,yearname]==years[i] & tdat[,dayname]>=starts[i] & tdat[,dayname]<=ends[i])
         tdat[rows,tnames]
@@ -58,12 +86,12 @@ rmsd <- function(fit, dat, na.rm=FALSE) {
 		fit1 <- fit[-frows]
 
 		drows <- which(is.na(dat))
-		dat1 <- dat[-drows]	
+		dat1 <- dat[-drows]
 	} else {
 		fit1 <- fit
 		dat1 <- dat
 	}
 
-	rmsd <- sqrt(sum((fit1-dat1)^2)/length(fit1))	
+	rmsd <- sqrt(sum((fit1-dat1)^2)/length(fit1))
 	return(rmsd)
 }
