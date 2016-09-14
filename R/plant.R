49f076eff44c6e745e2f2ff5fc50e6aff1a3d95b
elisehellwig <echellwig@ucdavis.edu>
2016-09-14 10:49:08 -0700

    started the documentation for parameterlist function

--- R/plant.R
+++ R/plant.R
@@ -1,3 +1,12 @@
-#' @include classes.R
+#' @include classes.R plantmethodsbasic.R parameterlistmethods.R
 
-#This script defines
+#This script defines functions to create objects of the ParameterList and Plant
+# classes
+
+#' Creates ParameterList object
+#'
+#' @param ct A list of cardinal temperatures
+#' @param length A numeric vector with the lengths of thermal time/day
+#'     accumulation. There should either be one entry for each stage or one
+#'     entry for all the stages.
+#'
