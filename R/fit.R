6bbdce5743f57137afa291ede7abbfd71b4dc3bd
elisehellwig <echellwig@ucdavis.edu>
2016-09-19 11:39:40 -0700

    outlining the fit function. still need to create the object that will be returned by fit.

--- R/fit.R
+++ R/fit.R
@@ -4,13 +4,25 @@
 #     objects.
 
 
-fit <- function(p, cores=NA) {
+fit <- function(p, lbounds, ubounds, cores=1L) {
 
     n <- stages(p)
 
     functionlist <- lapply(1:n, function(i) {
         objective <- function(x) {
-            return()
+            return(minrmse(p))
         }
     })
+
+    if (n > 1 & cores > 1) {
+
+        optimlist <- mclapply(1:length(functionlist), function(i) {
+            DEoptim(functionlist[[i]], lower=lbounds,upper=ubounds)$optim
+        }, mc.cores=cores)
+
+    } else {
+        optimlist <- lapply(1:length(functionlist), function(i) {
+            DEoptim(functionlist[[i]], lower=lbounds, upper=ubounds)$optim
+        })
+    }
 }
