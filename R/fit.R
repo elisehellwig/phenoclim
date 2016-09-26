337da1d539ca6f3a4613eed66997df841cb227bc
elisehellwig <echellwig@ucdavis.edu>
2016-09-26 09:31:13 -0700

    added an if statement to check if the bounds were of the right length

--- R/fit.R
+++ R/fit.R
@@ -1,15 +1,33 @@
-#' @include constructors.R yearsums.R
+#' @include constructors.R minrmse.R
 
 # This document contains the function that fits phenological models for Plant
 #     objects.
 
 
-fit <- function(p, lbounds, ubounds, cores=1L) {
+fit <- function(p, lbounds, ubounds, cores=1L, estimateCT=TRUE,
+                estimatelength=TRUE) {
+
+    if (!estimateCT & !estimatelength) {
+        stop('You at least estimate either the cardinal temperatures or the model length, though you can estimate both.')
+    }
+
+    if (lbounds != ubounds) {
+        stop('The lower and upper bound vectors must have the same number of parameters')
+    }
+
+    ttform <- form(p)
+
+    if (checkparlength(ttform, estimateCT, estimatelength)!=lbounds) {
+        stop(paste0('Your bounds have the wrong number of parameter values. ',
+                   'Your bounds are of length', length(lbounds),
+                   ', and they should be of length',
+                   checkparlength(ttform, estimateCT, estimatelength), '.'))
+    }
 
     n <- stages(p)
 
     functionlist <- lapply(1:n, function(i) {
-        objective(p, i)
+        objective(p, i, estimateCT, estimatelength)
     })
 
     if (n > 1 & cores > 1) {
