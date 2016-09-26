262d3f66823584a005e1b51df4289ef9f9e9802b
elisehellwig <echellwig@ucdavis.edu>
2016-09-25 20:06:36 -0700

    rewrote the minrmse functions to split them up by modtype and then wrote a wrapper for them. Also added documenation.

--- R/minrmse.R
+++ R/minrmse.R
@@ -1,44 +1,73 @@
-# This document contains functions that fit phenological models for Plant
-#     objects.
+#' @include thermalsum.R
 
+# This document contains functions that are minimized using the DEoptim
+#    function to find the optimum cardinal temperature and length of thermal
+#    accumulation
 
+#' Calculates partial model RMSE
+#'
+#' Calculates RMSE for the partial model of thermal time accumulation given a
+#'     certain set of cardinal temperatures and thermal time accumulation
+#'     length.
+#'
+#' @param pars Cardinal temperatures
+#' @param fdat the data.frame containing the phenological information
+#' @param tdat list containing the temperature information
+#' @param form the functional form of the thermal time accumulation
+#' @param length the length of thermal time accumulation (in days). It can be
+#'     either a set length of time (one number) or the total length of the
+#'     stage (one length for each entry in fdat).
+#' @param stage the number of the stage of the phenological model
+#' @return The RMSE value for a given set of cardinal temperatures and thermal
+#'     time accumulation length.
+minrmsepartial <- function(pars, fdat, tdat, form, length, stage) {
 
+    if (checkpars(pars)) {
+        tsums <- thermalsum(pars, fdat, tdat, 'partial', form, length, stage)
+        events <- paste0('event', stage:(stage+1))
 
-
-minrmsepartial <- function(pars, fdat, tdat, type='nocrit', sumlength=NA) {
-
-    if (length(pars)==2 & pars[1]>pars[2]) {
-        #print(2)
-        rmse <- Inf
-
-    } else if (length(pars)==3 & (pars[1]>pars[2]| pars[2]>pars[3]|pars[1]>pars[3])) {
-        #print(3)
-        rmse <- Inf
-
-
-    } else if (length(pars)==4 & ( pars[1]>pars[2]| pars[2]>pars[3]|pars[1]>pars[3]|pars[1]>pars[4]| pars[2]>pars[4]|pars[3]>pars[4])) {
-        #print(4)
-        rmse <- Inf
-
-    } else if (length(pars)>4) {
-        stop('There are no models with more than four parameters')
+        stagelength <- fdat[,events[2]] - fdat[,events[1]]
+        mod <- lm(stagelength ~ tsums)
+        fit <- fitted(mod)
+        rmse <- rmsd(fit, stagelength)
 
     } else {
-        ysums <- thermalsum(pars, fdat, tdat, typ=type, sumlen=sumlength,
-                          fn=flowername, hn=harvestname)
-
-
-        season <- fdat[,'slen']
-        mod <- lm(season ~ ysums)
-        fit <- fitted(mod)
-        rmse <- rmsd(fit, season)
+        rmse <- Inf
     }
 
-    if (!negative) {
+    return(rmse)
 
-        if (pars[1]<0) {
-            rmse <- def.rmse
-        }
+}
+
+
+
+#' Calculates full model RMSE
+#'
+#' Calculates RMSE for the full model of thermal time accumulation given a
+#'     certain set of cardinal temperatures and thermal time accumulation
+#'     length.
+#'
+#' @param pars Cardinal temperatures
+#' @param fdat the data.frame containing the phenological information
+#' @param tdat list containing the temperature information
+#' @param form the functional form of the thermal time accumulation
+#' @param length the length of thermal time accumulation (in days). It can be
+#'     either a set length of time (one number) or the total length of the
+#'     stage (one length for each entry in fdat).
+#' @param stage the number of the stage of the phenological model
+#' @return The RMSE value for a given set of cardinal temperatures and thermal
+#'     time accumulation length.
+minrmsefull <- function(pars, fdat, tdat, form, length, stage) {
+
+    if (checkpars(pars)) {
+
+        predictedlength <- thermalsum(pars, fdat, tdat, 'full', form, length,
+                                      stage)
+        observedlength <- eventi(fdat, stage+1) - eventi(fdat, stage)
+        rmse <- rmsd(predictedlength, observedlength)
+
+    } else {
+        rmse <- Inf
     }
 
 
@@ -47,127 +76,95 @@ minrmsepartial <- function(pars, fdat, tdat, type='nocrit', sumlength=NA) {
 }
 
 
+#' Calculates combined model RMSE
+#'
+#' Calculates RMSE for the combined model of thermal time accumulation given a
+#'     certain set of cardinal temperatures and thermal time accumulation
+#'     length.
+#'
+#' @param pars Cardinal temperatures
+#' @param fdat the data.frame containing the phenological information
+#' @param tdat list containing the temperature information
+#' @param form the functional form of the thermal time accumulation
+#' @param length the length of thermal time accumulation (in days). It can be
+#'     either a set length of time (one number) or the total length of the
+#'     stage (one length for each entry in fdat).
+#' @param stage the number of the stage of the phenological model
+#' @return The RMSE value for a given set of cardinal temperatures and thermal
+#'     time accumulation length.
+minrmsecomb <- function(pars, fdat, tdat, form, length, stage) {
 
-
-minrmsefull <- function(pars, fdat, tdat, type='nocrit', end=336,
-                       def.rmse=Inf, flowername='flower',
-                       harvestname='harvest', replaceinf=Inf) {
-
-    if (type=='anderson') {
-
-        fittedslen <- yeargd(pars, fdat, tdat, 'anderson', fn=flowername,
-                             hn=harvestname, end=end, replaceinf=replaceinf)
-
-
-        observedslen <- fdat[, 'slen']
-
-        rmse <- rmsd(fittedslen, observedslen)
-
-    } else if (length(pars)==3 & pars[2]>pars[3]) {
+    if (checkpars(pars)) {
         #print(2)
-            rmse <- def.rmse
-
-    } else if (length(pars)==4 & (pars[2]>pars[3]| pars[3]>pars[4]|pars[2]>pars[4])) {
-        #print(3)
-            rmse <- def.rmse
-
-
-    } else if (length(pars)==5 & ( pars[2]>pars[3] | pars[3]>pars[4] | pars[2]>pars[4]| pars[2]>pars[5] | pars[3]>pars[5] | pars[4]>pars[5])) {
-        #print(4)
-            rmse <- def.rmse
-
-    } else if (length(pars)>5) {
-        stop('There are no models implemented with more than 5 parameters.')
-
-    }  else {
-
-        fittedslen <- yeargd(pars, fdat, tdat, type, fn=flowername,
-                             hn=harvestname, end=end, replaceinf=replaceinf)
-
-
-        observedslen <- fdat[, 'slen']
-
-        rmse <- rmsd(fittedslen, observedslen)
-    }
-
-
-    return(rmse)
-
-}
-
-
-
-minrmsecomb <- function(pars, fdat, tdat, type='nocrit', end=336,
-                        def.rmse=Inf, negative=FALSE, flowername='flower',
-                        harvestname='harvest', replaceinf=Inf) {
-
-    if (length(pars)==3 & pars[2]>pars[3]) {
-        #print(2)
-        rmse <- def.rmse
-
-    } else if (length(pars)==4 & (pars[2]>pars[3]| pars[3]>pars[4]|pars[2]>pars[4])) {
-        #print(3)
-        rmse <- def.rmse
-
-
-    } else if (length(pars)==5 & ( pars[2]>pars[3] | pars[3]>pars[4] | pars[2]>pars[4]| pars[2]>pars[5] | pars[3]>pars[5] | pars[4]>pars[5])) {
-        #print(4)
-        rmse <- def.rmse
-
-    } else if (length(pars)>5) {
-        stop('There are no models implemented with more than 5 parameters.')
+       rmse <- Inf
 
     }  else {
 
         #print(pars)
 
-        daymet <- yeargd(pars, fdat, tdat, type, fn=flowername,
-                             hn=harvestname, end=end, replaceinf=replaceinf)
+        daymet <- thermalsum(pars, fdat, tdat, 'combined', form, length, stage)
         #print(daymet)
 
         if (any(is.infinite(daymet))) {
-            rmse <- def.rmse
+            rmse <- Inf
 
 
         } else {
 
-            season <- fdat[, 'slen']
+            stagelength <- eventi(fdat, stage+1) - eventi(fdat, stage)
+            positive <- ifelse(daymet > eventi(fdat, stage+1), FALSE, TRUE)
 
-            mod <- lm(season ~ daymet)
-            fit <- fitted(mod)
-            rmse <- rmsd(fit, season)
+            if (all(positive)) {
+                mod <- lm(stagelength ~ daymet)
+                fit <- fitted(mod)
+                rmse <- rmsd(fit, stagelength)
+
+            } else {
+                rmse <- Inf
+            }
 
         }
-
     }
-
-
 
     return(rmse)
 
 }
 
 
+#' Calculates model RMSE
+#'
+#' Calculates RMSE for a partial, full or combined model of thermal time
+#'     accumulation given a certain set of cardinal temperatures and thermal
+#'     time accumulation length.
+#'
+#' @param pars Cardinal temperatures
+#' @param fdat the data.frame containing the phenological information
+#' @param modtype character, the type of model. Can be 'partial', 'full' or
+#'     'combined'.
+#' @param tdat list containing the temperature information
+#' @param form the functional form of the thermal time accumulation
+#' @param length the length of thermal time accumulation (in days). It can be
+#'     either a set length of time (one number) or the total length of the
+#'     stage (one length for each entry in fdat).
+#' @param stage the number of the stage of the phenological model
+#' @return The RMSE value for a given set of cardinal temperatures and thermal
+#'     time accumulation length.
+#' @export
+minrmse <- function(pars, fdat, tdat, modtype, form, length, stage) {
 
-fit <- function(plant){
+    if (modtype=='partial') {
+        rmse <- minrmsepartial(pars, fdat, tdat, form, length, stage)
 
-}
+    } else if (modtype == 'full') {
+        rmse <- minrmsefull(pars, fdat, tdat, form, length, stage)
 
-
-
-minrmse <- function(pars, fdat, tdat, modtype, form, length) {
-
-    if (type=='partial') {
-        minrmsepartial(pars, fdat, tdat, form, length)
-
-    } else if (type == 'full') {
-        minrmsefull()
-
-    } else if (type == 'combined') {
-        minrmsecombined()
+    } else if (modtype == 'combined') {
+        minrmsecombined(pars, fdat, tdat, form, length, stage)
     } else {
         stop('Only options for model types are partial, full, and combined.')
     }
+
+    return(rmse)
 
 }
 
