f9a6e1283c82e303581bb4eb801aba7b7216a531
elisehellwig <echellwig@ucdavis.edu>
2016-09-25 17:48:50 -0700

    changed how I structured the phenology data frame and the minrmse arguments

--- R/objectivefunction.R
+++ R/objectivefunction.R
@@ -2,8 +2,11 @@ objective <- function(p, i) {
 
     pars <- cardinaltemps(p)[[i]]
     ml <- modlength(p)[i]
+    events <- paste0('event', i:(i+1))
 
-    fdat <- phenology(p)[, c('year',paste0('event', i:(i+1)))]
+    fdat <- phenology(p)[, c('year', events)]
+    fdat$stagelength <- fdat[,events[2]] - fdat[,events[1]]
+
 
     if (stages(p)==1) {
         tdat <- temperature(p)
@@ -12,7 +15,7 @@ objective <- function(p, i) {
     }
 
     fun <- function(x) {
-        return(minrmse(x , fdat, tdat, modtype(p), form(p)))
+        return(minrmse(x, fdat, tdat, modtype(p), form(p), i))
     }
 
 }
