3a7c32eb7412d53b569737a597be43523388a1f3
elisehellwig <echellwig@ucdavis.edu>
2016-10-04 15:11:22 -0700

    rewrote the growthmodel class to be the plantmodel class, and generics for the class.

--- R/plantmodelclass.R
+++ R/plantmodelclass.R
@@ -12,10 +12,67 @@
 #' @slot stages An integer with the number of stages in the phenological model
 #' @slot parameters A ParameterList object that contains the fit parameters.
 #' @export
-setClass('GrowthModel',
-         slots=list(phenology='data.frame',
-                    temperature='list',
-                    modeltype='character',
+setClass('PlantModel',
+         slots=list(parameters='ParameterList',
+                    error='numeric',
                     form='character',
+                    modeltype='character',
+                    simplified='logical',
                     stages='integer',
-                    parameters='ParameterList'))
+                    data='list'))
+
+#############################################################
+#Generics
+
+#' Returns the RMSE for a phenological model
+#'
+#' @param object An object of class PlantModel
+#' @return The root mean squared error (RMSE) of the model
+#' @export
+setGeneric('error', function(object) standardGeneric('error'))
+
+
+#' Is the PlantModel fit with the simplified model?
+#'
+#' @param object An object of class PlantModel
+#' @return logical, is the PlantModel the simplified version of the model?
+#' @export
+setGeneric('simplified', function(object) standardGeneric('simplified'))
+
+#' Returns the RMSE for a phenological model
+#'
+#' @param object An object of class PlantModel
+#' @return The root mean squared error (RMSE) of the model
+#' @export
+setGeneric('error', function(object) standardGeneric('error'))
+
+#' Returns the phenology data.frame
+#'
+#' @param object An object of class PlantModel
+#' @return A data.frame with the phenology data
+#' @export
+setGeneric('phenology', function(object) standardGeneric('phenology'))
+
+#' Returns the temperature list
+#'
+#' @param object An object of class PlantModel
+#' @return A list with the temperature data
+#' @export
+setGeneric('temperature', function(object) standardGeneric('temperature'))
+
+#' Displays the model type
+#'
+#' @param object An object of class PlantModel
+#' @return Returns the model type (character)
+#' @export
+setGeneric('modeltype', function(object) standardGeneric('modeltype'))
+
+#' Displays the number of stages in the phenological model
+#'
+#' @param object An object of class PlantModel
+#' @return The number of stages in the phenological model (numeric)
+#' @export
+setGeneric('stages', function(object) standardGeneric('stages'))
+
+
+
