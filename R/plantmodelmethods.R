c695b2be23db26578eeb2621e8e51df665b5dbc3
elisehellwig <echellwig@ucdavis.edu>
2016-10-06 14:05:28 -0700

    updated the plant model validity method

--- R/plantmodelmethods.R
+++ R/plantmodelmethods.R
@@ -115,6 +115,9 @@ setMethod("estimate", "PlantModel",
           function(object) {
               return(estimate(object@parameters))
           })
+
+
+
 ################################################################
 #validity method
 
@@ -125,23 +128,18 @@ setValidity("PlantModel", function(object) {
     n <- stages(object)
     temp <- temperature(object)
     frm <- form(object)
+    pheno <- phenology(object)
+    mt <- modeltype(object)
 
-    if (!(modeltype %in% c('partial','full','combined','time'))) {
+    if (!modeltypecheck(mt)[1]) {
         valid <- FALSE
-        msg <- c(msg,
-                 "modeltype must be either partial, full, combined, or time")
+        msg <- c(msg, modeltypecheck(mt)[1])
     }
 
-    pnames <- c('year', paste0('event', 1:n), paste0('length', 1:(n-1)))
-    missingcols <- setdiff(pnames, names(phenology(object)))
-
-    if (length0(missingcols)) {
-        valid <- FALSE
-        msg <- c(msg,
-                 paste("You are missing the following variables in your phenology data.frame:", diff))
-
-    }
-
+   if (!phenologycheck(pheno)[1]) {
+       valid <- FALSE
+       msg <- c(msg, phenologycheck[-1])
+   }
 
     if (!(checktempyears(object)[[1]])) {
         valid <- FALSE
@@ -157,21 +155,9 @@ setValidity("PlantModel", function(object) {
                  'The number of stages is not the same as the number of parameter value sets.')
     }
 
-    if (frm %in% c('gdd','gddsimple')) {
-
-        if (is.list(temp[[1]])) {
-            if (!is.data.frame(temp[[1]][[1]]) | dim(temp[[1]][[1]])[2] != 2) {
-                valid <- FALSE
-                msg <- c(msg,
-                         'Min and max temperatures are required for gdd and gddsimple models.')
-            }
-        } else {
-            if (!is.data.frame(temp[[1]]) | dim(temp[[1]])[2] != 2) {
-                valid <- FALSE
-                msg <- c(msg,
-                         'Min and max temperatures are required for gdd and gddsimple models.')
-            }
-        }
+    if (!temptypecheck(temp)[1]) {
+        valid <- FALSE
+        msg <- c(msg, temptypecheck(temp)[-1])
     }
 
     if (valid) TRUE else msg
