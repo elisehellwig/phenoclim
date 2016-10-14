8427aa563302dfaf1ee61aee92e81d6f4349aff2
elisehellwig <echellwig@ucdavis.edu>
2016-10-14 10:23:41 -0700

    updating documentation

--- R/crossval.R
+++ R/crossval.R
@@ -4,4 +4,15 @@ NULL
 
 
 
-crossval()
+
+
+#' Crossvalidates the PlantModel Error
+#'
+#' Does k-fold crossvalidation on the error of a PlantModel object.
+#'
+#' @param plant PlantModel, the object to be crossvalidated
+#' @param k numeric, number of folds for crossvalidation.
+#' @return A PlantModel object with the error slot having the new crossvalidated
+#'     error
+#' @export
+crossval <- function(plant) {}
