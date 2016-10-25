19b85033ede8d03f0883f650be0ed3abbd5cabec
elisehellwig <echellwig@ucdavis.edu>
2016-10-24 20:49:39 -0700

    rewrote the crossval function so there is only one. now only works with one stage right now. bleh

--- R/crossval.R
+++ R/crossval.R
@@ -1,23 +1,40 @@
 #' @include plantmodel.R
 NULL
 
-#' Crossvalidating one stage
+
+#' Crossvalidates the PlantModel Error
 #'
-#' @param k numeric, the number of folds in the crossvalidation.
-#' @param fun character, the name of the function used to evaluate the goodness
-#'     of fit.
-#' @param stage numeric, the stage of the phenology model.
-#' @param pdat data.frame, the phenology data.frame.
-#' @param temps data.frame, the temperature data.frame
-#' @param parlist list, list of parameterlists
+#' Does k-fold crossvalidation on the error of a PlantModel object.
+#'
+#' @param plant PlantModel, the object to be crossvalidated
+#' @param k numeric, number of folds for crossvalidation.
+#' @param temps data.frame, contains all the temperature data.
+#' @param seed numeric, the number to be used as the seed. Set to NA for no seed
+#'     set.
+#' @param fun character, the name of the function used to evaluate the model.
 #' @param lbounds numeric, lower bounds of parameters
 #' @param ubounds numeric, upper bounds of parameters.
 #' @param cores integer, number of cores to be used when fitting the models
 #' @param iterations numeric, number of iterations to be used when optimizing
 #'     paramters.
-#' @return The mean rmse
-stagecrossval <- function(k, fun, stage, pdat, temps, parlist, lbounds, ubounds,
-                          cores=1L, iterations=100) {
+#' @return A PlantModel object with the error slot having the new crossvalidated
+#'     error
+#' @export
+crossval <- function(plant, temps, k, seed, fun='rmsd', lbounds, ubounds,
+                     iterations=100, cores=1L) {
+
+    parlist <- parameters(plant)
+    m <- length(parlist)
+    p <- phenology(plant)
+    stage <- 1
+
+
+    if (is.numeric(seed)) {
+        set.seed(seed)
+    }
+
+    p$fold <- kfold(p, k=k)
+
     m <- length(parlist)
     measure <- matrix(rep(NA, m*k), nrow=m)
 
@@ -43,13 +60,11 @@ stagecrossval <- function(k, fun, stage, pdat, temps, parlist, lbounds, ubounds,
 
         response <- paste0('length', stage)
 
-
-
         testdata <- lapply(1:length(parlist), function(j) {
             pl <- parlist[[j]]
             as.data.frame(thermalsum(cardinaltemps(pl)[stage], test,
-                        whichtemp(ttforms[j], daytemplist, hourtemplist),
-                        modeltype(pl),ttforms[j], modlength(pl)[j],stage))
+                         whichtemp(ttforms[j], daytemplist, hourtemplist),
+                         modeltype(pl),ttforms[j], modlength(pl)[j],stage))
         })
 
 
@@ -69,42 +84,11 @@ stagecrossval <- function(k, fun, stage, pdat, temps, parlist, lbounds, ubounds,
 
     avgmeasure <- apply(measure, 1, mean)
 
-    return(avgmeasure)
-}
 
 
 
-#' Crossvalidates the PlantModel Error
-#'
-#' Does k-fold crossvalidation on the error of a PlantModel object.
-#'
-#' @param plant PlantModel, the object to be crossvalidated
-#' @param k numeric, number of folds for crossvalidation.
-#' @param temps data.frame, contains all the temperature data.
-#' @param seed numeric, the number to be used as the seed. Set to NA for no seed
-#'     set.
-#' @param fun character, the name of the function used to evaluate the model.
-#' @return A PlantModel object with the error slot having the new crossvalidated
-#'     error
-#' @export
-crossval <- function(plant, temps, k, seed, fun='rmsd') {
 
-    m <- length(parameters(plant))
-    p <- phenology(plant)
-
-    if (is.numeric(seed)) {
-        set.seed(seed)
-    }
-
-    p$fold <- kfold(p, k=k)
-
-    CVerrors <- sapply(1:m, function(j) {
-        sapply(1:stages(plant), function(i) {
-            stagecrossval(p, modeltype(plant), k, i, fun)
-        })
-    })
-
-    error(plant) <- CVerrors
+    error(plant) <- avgmeasure
     crossvalidated(plant) <- TRUE
 
     return(plant)
