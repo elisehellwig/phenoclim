0c99d55a907e14bb4c76f63229aca048b9d14dd2
elisehellwig <echellwig@ucdavis.edu>
2016-11-02 16:04:39 -0700

    debugging changes

--- R/crossval.R
+++ R/crossval.R
@@ -75,10 +75,13 @@ crossval <- function(plant, temps, k, seed, fun='rmsd', lbounds, ubounds,
             names(testdata[[j]]) <- predictors[[j]]
         }
 
+        print(trainmod[[1]])
+
         fit <- lapply(1:m, function(j) {
             predict(trainmod[[j]][[stage]], newdata=testdata[[j]])
         })
 
+        print(7)
         #str(fit)
 
 
