af4e025fb9f9b920e9f8a1bf53afd6ce3ae8cd6b
elisehellwig <echellwig@ucdavis.edu>
2018-02-22 09:38:10 -0800

    methods for the new flowermodel class

---
+++ R/flowermodelmethods.R
@@ -0,0 +1,222 @@
+#' @include plantmodelclass.R flowermodelclass.R
+
+# methods for the FlowerModel class
+
+
+setMethod("show",
+          signature = 'FlowerModel',
+          definition = function(object) {
+
+              n <- object@parameters[[1]]@stages #extract stages
+              pheno <- object@phenology #extract phenology data frame
+              eventcols <- paste0('event', 1:(n+1)) #create event column names
+              lengthcols <- paste0('length', 1:(n)) #create length column names
+              m <- length(object@parameters) #number of parameters
+
+              parshow <- ldply(object@parameters, function(pl) {
+                  showparlist(pl)
+              }) #create parameter lists to display
+
+              parshow$model <- rep(1:m, each=n) #creating model numbers
+              parshow$error <- round(as.numeric(object@error),2) #rounding error
+                                                                 #values
+              #taking the average dates for each of the phenological
+                #events
+              avgdates <- round(apply(pheno[,eventcols], 2, mean))
+
+              #calculating the average lengths for each of the stages
+              if (is.numeric(pheno[,lengthcols])) {
+                  avglengths <- round(mean(pheno[,lengthcols]))
+              } else {
+                  avglengths <- round(apply(pheno[,lengthcols], 2, mean))
+              }
+
+              #more statistics on the data
+              rng <- range(pheno[,'year'])
+              span <- rng[2] - rng[1]
+              obs <- nrow(pheno)
+
+              #checking to see if model is cross validated and saving the result
+              if (object@crossvalidated) cv <-'is' else cv <- 'is not'
+
+              #creating what is displayed
+              cat('Stages; stage lengths; event days: ', n,'; ' ,
+                  paste(avglengths, collapse=', '), '; ',
+                  paste(avgdates, collapse=', '), '\n', sep='')
+              #cat('The data spans ', span, ' years, and has ', obs,
+               #   ' observations.', '\n', sep='')
+              cat('Model error is in days and ', cv, ' crossvalidated.', '\n',
+                  sep='')
+              cat('Model Parameters:','\n', sep='')
+              print(parshow)
+
+          })
+
+################################################################
+#Accessor methods
+##top level
+
+#' Accesses the parameters of a FlowerModel object
+#' @rdname parameters
+setMethod("parameters", "FlowerModel",
+          function(object) {
+              return(object@parameters)
+          })
+
+
+#' Accesses the RMSE of a FlowerModel object
+#' @rdname error
+setMethod("error", "FlowerModel",
+          function(object) {
+              return(object@error)
+          })
+
+
+#' Accesses the phenology data.frame of a FlowerModel object
+#' @rdname phenology
+setMethod("phenology", "FlowerModel",
+          function(object) {
+              return(object@phenology)
+          })
+
+
+
+#' Accesses the linear models of a FlowerModel object
+#' @rdname olm
+setMethod("olm", "FlowerModel",
+          function(object) {
+              return(object@olm)
+          })
+
+#' Is the model error crossvalidated?
+#' @rdname crossvalidated
+setMethod("crossvalidated", "FlowerModel",
+          function(object) {
+              return(object@crossvalidated)
+          })
+
+
+######Paremeterlist accessors
+
+#' Accesses the number of stages of a FlowerModel object
+#' @rdname stages
+setMethod("stages", "FlowerModel",
+          function(object) {
+              return(object@parameters[[1]]@stages)
+          })
+
+#' Accesses form of a FlowerModel object
+#' @rdname form
+setMethod("form", "FlowerModel",
+          function(object) {
+              frms <- lapply(object@parameters, function(parlist) {
+                  #print(class(parlist))
+                  form(parlist)
+              })
+              return(frms)
+          })
+
+#' Accesses the model type of a FlowerModel object
+#' @rdname modeltype
+setMethod("modeltype", "FlowerModel",
+          function(object) {
+
+              return(object@parameters[[1]]@modeltype)
+          })
+
+
+#' Accesses the cardinal temperatures
+#' @rdname cardinaltemps
+setMethod("cardinaltemps", "FlowerModel",
+          function(object) {
+
+              ct <- lapply(object@parameters, function(parlist) {
+                  parlist@cardinaltemps
+              })
+
+              return(ct)
+          })
+
+#' Accesses the model lengths of a FlowerModel object
+#' @rdname modlength
+setMethod("modlength", "FlowerModel",
+          function(object) {
+              ml <- lapply(object@parameters, function(parlist) {
+                  parlist@modlength
+              })
+              return(ml)
+          })
+
+#' Accesses what parameters are to be optimized
+#' @rdname parsOptimized
+setMethod("parsOptimized", "FlowerModel",
+          function(object) {
+              pO <- lapply(object@parameters, function(parlist) {
+                  parlist@parsOptimized
+              })
+              return(pO)
+          })
+
+
+
+################################################################
+#validity method
+
+setValidity("FlowerModel", function(object) {
+    msg <- NULL
+    valid <- TRUE
+
+    #print(5)
+    n <- stages(object)
+    frm <- form(object)
+    pheno <- phenology(object)
+    mt <- modeltype(object)
+
+    if (!modeltypecheck(mt)[1]) {
+        valid <- FALSE
+        msg <- c(msg, modeltypecheck(mt)[1])
+    }
+
+   if (!phenologycheck(n, pheno)[1]) {
+       valid <- FALSE
+       msg <- c(msg, phenologycheck[-1])
+   }
+
+
+    if (length(modlength(object@parameters[[1]])) != n) {
+        valid <- FALSE
+        msg <- c(msg,
+                 'The number of stages is not the same as the number of parameter value sets.')
+    }
+
+
+    if (valid) TRUE else msg
+
+})
+
+#############################
+#setting stuff
+
+#' @rdname crossvalidated-set
+setMethod('crossvalidated<-', 'FlowerModel',
+          function(object, value) {
+              object@crossvalidated <- value
+
+              if (validObject(object)) {
+                  return(object)
+              }
+          })
+
+
+#' @rdname error-set
+setMethod('error<-', 'FlowerModel',
+          function(object, value) {
+              object@error <- value
+
+              if (validObject(object)) {
+                  return(object)
+              }
+          })
+
+
+
