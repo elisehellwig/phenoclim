e73b2c718dc209843775179c0f60fee72dda9085
elisehellwig <echellwig@ucdavis.edu>
2018-05-10 09:50:25 -0700

    updated the main minrmse function to reflect the new nomenclature, have yet to do the subfunctions.

--- R/minrmse.R
+++ R/minrmse.R
@@ -22,10 +22,13 @@ NULL
 #' @param modclass character, type of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
+#' @param firstevent character, the name of the column that contains the
+#'     starting phenological event for the model. For FlowerModels this is the
+#'     harvest date.
 #' @return The RMSE value for a given set of cardinal temperatures and thermal
 #'     time accumulation length.
 minrmseDT <- function(pars, fdat, tdat, form, thresh, stage, varying,
-                      modclass) {
+                      modclass, firstevent) {
 
     responsename <- responseVar(modclass, stage)
 
@@ -178,22 +181,24 @@ minrmseTTT <- function(pars, fdat, tdat, form, length, stage, modclass) {
 #' @param stage the number of the stage of the phenological model
 #' @param CT logical, should the cardinal temperatures be optimized. If not CT
 #'     is the vector of cardinal temperatures.
-#' @param S logical, is the day to start counting being optimized? (ie.
-#'     not using bloom as startday for PlantModels and Harvest for FlowerModels)
-#' @param TH logical, should the model accumulation threshold be optimized. If
-#'     not L is the (numeric) model accumulation threshold.
+#' @param S logical/numeric, is the day to start counting being optimized? If
+#'     not, the value associated with the start day should be provided.
+#' @param TH logical/numeric, should the model threshold be optimized. If
+#'     not L is the (numeric) model threshold.
 #' @param simple logical, should the simplified version of the model be run?
 #' @param varying character, c('start', 'threshold') should either of these pars
 #'     vary from year to year.
-#' @param modclass logical, Do count forward from the starting event (as
-#'     opposed to backward)? If you have negative values in your event days
-#'     you forward should probably be FALSE. Bloom model forward=false, harvest
-#'     model forward=true.
-#' @return The RMSE value for a given set of cardinal temperatures and thermal
-#'     time accumulation length.
+#' @param modclass character, type of model to be estimating, options are
+#'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
+#'     probably want flower model.
+#' @param firstevent character, the name of the column that contains the
+#'     starting phenological event for the model. For FlowerModels this is the
+#'     harvest date.
+#' @return The RMSE value for a given set of cardinal temperatures start day
+#'     model threshold.
 #' @export
 minrmse <- function(pars, fdat, tdat, modtype, form, stage, CT, S, TH, simple,
-                    varying, modclass) {
+                    varying, modclass, firstevent) {
 
     ctlen <- parnum(form) #what is the number of parameter values for the form
     plen <- length(pars)
@@ -205,30 +210,13 @@ minrmse <- function(pars, fdat, tdat, modtype, form, stage, CT, S, TH, simple,
     if (isTRUE(CT)) ct <- pars[(plen-ctlen+1):plen] else ct <- CT[1:ctlen]
 
 
-    #is the threshold estimated
-    if (!isTRUE(TH)) {#case 2: not optimizing threshold
-        th <- TH
-
-    } else if (isTRUE(S)) { #case 2: optimizing threshold and start day
-        th <- pars[2]
-
-    } else {
-        th <- pars[1] #case 3: optimizing threshold, start day not estimated
-    }
 
 
-    #is the startday estimated
-    if (isTRUE(S)) {
-        s <- pars[1]
+    startthresh <- convertParameters(pars, modtype, S, TH, varying,
+                                     fdat[,firstevent])
 
-    } else if (is.na(S)) { #case 2: not estimating start day, start day is
-        s <- fdat[,events[1]]   #the earlier event, harvest for predicting
-                                #bloom, bloom for predicting season length
-
-    } else {
-        s <- S #case 3: not optimizing start day
-    }                          #start day is a constant day of the year
-
+    s <- startthresh[1]
+    th <- startthresh[2]
 
     #print(simple)
 
@@ -236,14 +224,15 @@ minrmse <- function(pars, fdat, tdat, modtype, form, stage, CT, S, TH, simple,
 
 
     if (modtype=='DT' & !simple) {
-        rmse <- minrmseDT(ct, fdat, tdat, form, th, stage, varying, modclass)
+        rmse <- minrmseDT(ct, fdat, tdat, form, s, th, stage, varying, modclass)
 
     } else if (modtype == 'TTT' & simple) {
-        rmse <- minrmseTTTsimplified(ct, fdat, tdat, form, th, stage, varying,
-                                     modclass)
+        rmse <- minrmseTTTsimplified(ct, fdat, tdat, form, s, th, stage,
+                                     varying, modclass)
 
     } else if (modtype == 'TTT') {
-        rmse <- minrmseTTT(ct, fdat, tdat, form, th, stage, varying, modclass)
+        rmse <- minrmseTTT(ct, fdat, tdat, form, s, th, stage, varying,
+                           modclass)
     } else {
         stop('Only options for modeltypes are TTT and DT.')
     }
