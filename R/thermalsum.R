ff3b9cc4ff4c33d18fd09ae18e050033fc8bc12d
elisehellwig <echellwig@ucdavis.edu>
2018-05-14 12:08:20 -0700

    In the process of updating the thermal sum family of functions.

--- R/thermalsum.R
+++ R/thermalsum.R
@@ -8,68 +8,31 @@ NULL
 #' Calculates thermal time sums for a given length of days after flowering for
 #' a series of years.
 #'
-#' @param pars Cardinal temperatures
+#' @param ctemps Cardinal temperatures
 #' @param fdat the data.frame containing the phenological information
 #' @param tdat list containing the temperature information
 #' @param form the functional form of the thermal time accumulation
-#' @param length the length of thermal time accumulation (in days). It can be
-#'     either a set length of time (one number) or the total length of the
-#'     stage (one length for each entry in fdat).
-#' @param lims numeric, start/stop pair if no length or start day.
+#' @param start numeric, the day to start accumulating time or thermal time
+#'     towards the model threshold.
+#' @param end numeric, the length of thermal time accumulation, in days.
 #' @param stage the number of the stage of the phenological model.
-#' @param varying character, c('start', 'threshold') should either of these pars
-#'     vary from year to year.
 #' @param mclass character, class of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
 #' @return The thermal sums for a given series of years.
-DTsum <- function(pars, fdat, tdat, form, length, lims, stage, mclass) {
-
-
-    if (mclass=='FlowerModel') {
-        start <- 1 # all FMs start at 1 because we use an index
-
-
-        if (any(is.na(lims))) { #if both limits aren't present
-                                #we end at flowering
-            yrlen <- ifelse(is.leapyear(fdat$year), 366, 365) #year lengths
-            shift <- yrlen - which(!is.na(lims))#What is the shift?
-            end <- fdat[,'event1'] + shift # index of day of flowering
-        } else {
-
-        }
-        end <- length #otherwise we end at the end of the length
-
-    } else {
-        #the day of the starting event
-        start <- fdat[, paste0('event',stage)]
-
-        #the start + the length of the model/threshold
-        end <- start+length
-
-        }
+DTsum <- function(ctemps, fdat, tdat, form, start, end, stage, mclass) {
 
 	# for walnut
 	#fdat is data for the 'fruit'
     years <- fdat[,'year'] #extracting the years we are interested in
 
     if (form %in% c('gdd','gddsimple')) { #if it is a GDD model
-
-        #create the index of days we want temperatures for
-        tempindex <- startEnd(start, end, hourly=FALSE, forward=forward)
-
+        startindex <- start #keep days as days
+        endindex <- end
 
     } else { #if it is a GDH model
-        start <- (start*24)-23 #convert days to hours
-
-        if (stgtype=='PlantModel') {# convert days to hours
-            end <- start+length*24 #adding for plant model
-        } else {
-            end <- start-length*24 #subtracting for flower model
-        }
-
-        #create the index of hours we want temperatures for
-        tempindex <- startEnd(start, end, hourly=TRUE, forward=forward)
+        startindex <- (start*24)-23 #convert days to hours
+        startindex <- start+end*24
 
     }
 
@@ -120,15 +83,16 @@ DTsum <- function(pars, fdat, tdat, form, length, lims, stage, mclass) {
 #' @param fdat the data.frame containing the phenological information
 #' @param tdat list containing the temperature information
 #' @param form the functional form of the thermal time accumulation
-#' @param length the length of thermal time accumulation (in days). It can be
-#'     either a set length of time (one number) or the total length of the
-#'     stage (one length for each entry in fdat).
+#' @param start numeric, the day to start accumulating time or thermal time
+#'     towards the model threshold.
+#' @param thresh numeric, the length of thermal time accumulation (in either
+#'     days or thermal time units).
 #' @param stage the number of the stage of the phenological model
 #' @param forward logical, Do count forward from the starting event (as
 #'     opposed to backward)? If you have negative values in your event days
 #'     you forward should probably be FALSE.
 #' @return The thermal sums for a given series of years.
-TTTsum <- function(pars, fdat, tdat, form, length, stage, forward) {
+TTTsum <- function(pars, fdat, tdat, form, start, thresh, stage, forward) {
 
     #print(str(pars))
 
@@ -189,7 +153,7 @@ TTTsum <- function(pars, fdat, tdat, form, length, stage, forward) {
 #'
 #' Calculates thermal time sums for either the TTT or DT time model
 #'
-#' @param pars Cardinal temperatures
+#' @param ctemps Cardinal temperatures
 #' @param fdat the data.frame containing the phenological information
 #' @param tdat list containing the temperature information
 #' @param modtype character, specifies what type of model is being run. Can be
@@ -204,7 +168,7 @@ TTTsum <- function(pars, fdat, tdat, form, length, stage, forward) {
 #'     'PlantModel' or 'FlowerModel'.
 #' @return The thermal sums for a given series of years.
 #' @export
-thermalsum <- function(pars, fdat, tdat, modtype, form, start, thresh, stage,
+thermalsum <- function(ctemps, fdat, tdat, modtype, form, start, thresh, stage,
                        varying, mclass) {
 
     if (!(is.numeric(thresh) | is.integer(thresh))) {
@@ -212,10 +176,10 @@ thermalsum <- function(pars, fdat, tdat, modtype, form, start, thresh, stage,
     }
 
     if (modtype=='DT') {
-        ths <- DTsum(pars, fdat, tdat, form, start, thresh, stage, mclass)
+        ths <- DTsum(ctemps, fdat, tdat, form, start, thresh, stage, mclass)
 
     } else if (modtype=='TTT') {
-        ths <- TTTsum(pars, fdat, tdat, form, start, thresh, stage, mclass)
+        ths <- TTTsum(ctemps, fdat, tdat, form, start, thresh, stage, mclass)
         #print(ths)
 
     } else {
