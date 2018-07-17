f55bfc46d5fb7fdeb4f44de7a721160140e0bc3a
elisehellwig <echellwig@ucdavis.edu>
2018-07-17 14:08:54 -0700

    added function that convert days of the year to date based on year and modelclass

--- R/fithelper.R
+++ R/fithelper.R
@@ -1,5 +1,65 @@
-#' @include flipday.R
-NULL
+
+#' Identifies which year each day corresponds to
+#'
+#' This function decides whether a day happened in the same year as the
+#'     associated phenological event, or the year before.
+#'
+#' @param day numeric, a vector of start or threshold days
+#' @param years numeric, a vector of years of the phenological event
+#'     associated with the start days
+#' @param modclass character, PlantModel or FlowerModel
+#' @return A vector of years that correspond to the actual year each of the days
+#'     in the day vector happened.
+correctYears <- function(day, years, modclass) {
+
+    if (modclass=='FlowerModel') {
+        correctedYears <- ifelse(day>180, years-1, years)
+    } else {
+        correctedYears <- years
+    }
+
+    return(correctedYears)
+
+}
+
+
+
+#' Converts day of the year to date-time
+#'
+#' This function takes in a vector of days of the year and a vector of years
+#'     and converts them to a date-time vector of the class POSIXct
+#'
+#' @param years numeric, a vector of years to use for each day of the year
+#' @param days numeric, the day of the year you want to convert to date-time
+#' @param modclass character, 'PlantModel' or 'FlowerModel'.
+#' @param hours numeric, a vector of hours to use in your date-time conversion
+#' @param startTime character, used to specify the same time of day for each
+#'     date-time entry element. Only used if hours=NA.
+#' @return A POSIXct vector of date-times
+#' @export
+dayToDate <- function(years, days, modclass, hours=NA, startTime='00:00:00',
+                      timezone='America/Los_Angeles') {
+
+    cyears <- correctYears(days, years, modclass)
+
+    datestring <- as.Date(days-1, origin=paste0(cyears, '-01-01'))
+
+    if (is.na(hours[1])) {
+        dateTimeString <- paste0(datestring, ' ', startTime)
+
+    } else {
+        timestring <- paste0(sprintf("%02d", hours), ':00:00')
+        dateTimeString <- paste(datestring, timestring)
+
+    }
+
+
+    posix <- as.POSIXct(dateTimeString, "%Y-%m-%d %H:%M:%OS",
+                        tz=timezone)
+
+    return(posix)
+
+}
 
 
 
@@ -56,7 +116,10 @@ dayToDAE <- function(day, event, years) {
 #'     vary from year to year.
 #' @param eventvec numeric, a vector that contains the day of the year for the
 #'     starting event of the model. For bloom the event is harvest.
-convertParameters <- function(pars, modtype, S, TH, vp, eventvec, years) {
+#' @return list of the correct start day vector and the threshold vector based
+#'     on the type of model being run.
+convertParameters <- function(pars, modtype, S, TH, vp, eventvec, years,
+                              modclass) {
 
     #Estimating start day
     if (isTRUE(S)) {
@@ -65,6 +128,11 @@ convertParameters <- function(pars, modtype, S, TH, vp, eventvec, years) {
     } else { #Not estimating start day
         s <- S
 
+    }
+
+    #getting the correct years based on when the start day is
+    if (modclass=='FlowerModel') {
+        years <- ifelse(s>210, years-1, years)
     }
 
     #does start day vary from year to year?
