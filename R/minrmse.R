4a383fab147dec5f7a84f729e74e5d31f35be51b
elisehellwig <echellwig@ucdavis.edu>
2018-07-24 10:47:09 -0700

    updated it so that you can run minrmse with numeric start days as well.

--- R/minrmse.R
+++ R/minrmse.R
@@ -24,10 +24,11 @@ NULL
 #' @param modclass character, type of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
+#' @param startingevent numeric, days first event happened each year.
 #' @return The RMSE value for a given set of cardinal temperatures and thermal
 #'     time accumulation length.
 minrmseDT <- function(pars, fdat, tdat, form, start, thresh, stage, varying,
-                      modclass) {
+                      modclass, startingevent=NA) {
 
     print('minrmseDT')
     responsename <- responseVar(modclass, stage)
@@ -37,7 +38,7 @@ minrmseDT <- function(pars, fdat, tdat, form, start, thresh, stage, varying,
         #calculate the thermal sum for each year using the cardinal temps and
             #day threshold
         tsums <- thermalsum(pars, fdat$year, tdat, 'DT', form, start, thresh,
-                            varying, modclass)
+                            varying, modclass, startingevent)
 
         #use that data as a predictor in a model to predict stage length
         mod <- lm(fdat[,responsename] ~ tsums)
@@ -76,10 +77,11 @@ minrmseDT <- function(pars, fdat, tdat, form, start, thresh, stage, varying,
 #' @param modclass character, type of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
+#' @param startingevent numeric, days first event happened each year.
 #' @return The RMSE value for a given set of cardinal temperatures and thermal
 #'     time accumulation length.
 minrmseTTTsimplified <- function(pars, fdat, tdat, form, start, thresh, stage,
-                                 varying, modclass) {
+                                 varying, modclass, startingevent=NA) {
     print('minrmseTTTsimp')
 
     responsename <- responseVar(modclass, stage) #name of response variable
@@ -88,7 +90,7 @@ minrmseTTTsimplified <- function(pars, fdat, tdat, form, start, thresh, stage,
 
         #calculate day thermal time threshold is met
         daymet <- thermalsum(pars, fdat$year, tdat, 'TTT', form, start, thresh,
-                                      varying, modclass)
+                                      varying, modclass, startingevent)
 
         if (any(is.infinite(daymet))){ #were any of the thermal sums
             #that did not resolve
@@ -127,10 +129,11 @@ minrmseTTTsimplified <- function(pars, fdat, tdat, form, start, thresh, stage,
 #' @param modclass character, type of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
+#' @param startingevent numeric, days first event happened each year.
 #' @return The RMSE value for a given set of cardinal temperatures and thermal
 #'     time accumulation length.
 minrmseTTT <- function(pars, fdat, tdat, form, start, thresh, stage,
-                       varying, modclass) {
+                       varying, modclass, startingevent=NA) {
 
     print('minrmseTTT')
 
@@ -144,7 +147,7 @@ minrmseTTT <- function(pars, fdat, tdat, form, start, thresh, stage,
 
         #calculate day thermal time threshold is met
         daymet <- thermalsum(pars, fdat$year, tdat, 'TTT', form, start, thresh,
-                             varying, modclass)
+                             varying, modclass, startingevent)
         #print(daymet)
 
         if (any(is.infinite(daymet))) {
@@ -203,11 +206,14 @@ minrmseTTT <- function(pars, fdat, tdat, form, start, thresh, stage,
 #' @param firstevent character, the name of the column that contains the
 #'     starting phenological event for the model. For FlowerModels this is the
 #'     harvest date.
+#' @param startingevent numeric, days first event happened each year. Only
+#'     necessary if you are running minrmse on its own (not within the
+#'     flowermodel or plantmodel functions).
 #' @return The RMSE value for a given set of cardinal temperatures start day
 #'     model threshold.
 #' @export
 minrmse <- function(pars, fdat, tdat, modtype, form, stage, CT, S, TH, simple,
-                    varying, modclass, firstevent) {
+                    varying, modclass, firstevent, startingevent=NA) {
 
     ctlen <- parnum(form) #what is the number of parameter values for the form
     plen <- length(pars)
@@ -233,15 +239,16 @@ minrmse <- function(pars, fdat, tdat, modtype, form, stage, CT, S, TH, simple,
 
 
     if (modtype=='DT' & !simple) {
-        rmse <- minrmseDT(ct, fdat, tdat, form, s, th, stage, varying, modclass)
+        rmse <- minrmseDT(ct, fdat, tdat, form, s, th, stage, varying,
+                          modclass, startingevent)
 
     } else if (modtype == 'TTT' & simple) {
         rmse <- minrmseTTTsimplified(ct, fdat, tdat, form, s, th, stage,
-                                     varying, modclass)
+                                     varying, modclass, startingevent)
 
     } else if (modtype == 'TTT') {
         rmse <- minrmseTTT(ct, fdat, tdat, form, s, th, stage, varying,
-                           modclass)
+                           modclass, startingevent)
     } else {
         stop('Only options for modeltypes are TTT and DT.')
     }
