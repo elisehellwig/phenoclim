9340e546dab4c311e3946760cdd7c656a8b392fe
elisehellwig <echellwig@ucdavis.edu>
2018-12-12 15:07:49 -0800

    added some stuff for troubleshooting purposes

--- R/doublesum.R
+++ R/doublesum.R
@@ -12,26 +12,33 @@ NULL
 #' @param forms the functional forms of the thermal time accumulation
 #' @param startDate POSIXct, the date to start accumulating time or thermal
 #'     time towards the model threshold.
-#' @param thresh numeric, the length of thermal time accumulation (in either
+#' @param threshs numeric, the length of thermal time accumulation (in either
 #'     days or thermal time units).
 #' @param varying character, c('start', 'threshold') should either of these
 #'     pars vary from year to year.
 #' @param mclass character, class of model to be estimating, options are
 #'     'PlantModel' or 'FlowerModel'. If you have negative day values, you
 #'     probably want flower model.
+#' @param startingevent numeric, days first event happened each year.
 #' @return The thermal sums for a given series of years.
+#' @export
 doublesum <- function(pars, yrs, tdat, forms, startDate, thresh, varying,
-                      mclass) {
+                      mclass, startingevent=NA) {
 
 
     endchill <- thermalsum(pars[[1]], yrs, tdat, 'TTT', forms[1], startDate,
-                            thresh[1], NA, 'FlowerModel')
+                            thresh[1], NA, 'FlowerModel', startingevent)
+
+    print(endchill)
 
     heatStartDate <- dayToDate(yrs, endchill+1, 'FlowerModel')
+    print(heatStartDate)
 
-    endheat <- thermalsum(pars[[2]], yrs, tdat, 'TTT', forms[2], heatStartDate,
-                          thresh[2], NA, 'FlowerModel')
+    heatdays <- thermalsum(pars[[2]], yrs, tdat, 'TTT', forms[2], heatStartDate,
+                          thresh[2], NA, 'FlowerModel', startingevent)
 
+
+    endheat <- endchill + 1 + heatdays
 
     return(endheat)
 
